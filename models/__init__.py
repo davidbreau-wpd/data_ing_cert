8c14d3071b4d9b65d10ec9a9500df18bd5137a74
davidbreau <davidbreau@live.fr>
2025-04-30 13:59:27 +0200

    :construction: working on sql ingestion. Demo base ready for demo sql queries

--- models/__init__.py
+++ models/__init__.py
@@ -5,9 +5,12 @@ from .vestas_report import Vestas_Report, Vestas_Reports_Processor
 from .enercon_report import Enercon_Report, Enercon_Reports_Processor
 from .DataLake import DataLake
 
+from .database_manager import DatabaseManager
+
 
 __all__ = ['_Service_Report', 
            'Vestas_Report', 'Vestas_Reports_Processor', 
            'Enercon_Report', 'Enercon_Reports_Processor',
-           'Engine', 'Database'
-           'DataLake']
+           'Engine', 'Database',
+           'DataLake',
+           'Databasemanager']
